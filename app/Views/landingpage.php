0bc257756bb693965ad1bb2819474bf62e0f9288
Muhammad Dhika Azizi <mdhikas@gmail.com>
2021-01-07 15:05:31 +0700

    first commit

---
+++ app/Views/landingpage.php
@@ -0,0 +1,233 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Aiotech ITPLN</title>
+    <link rel="icon" type="image/x-icon" href="/assets/img/logos/aiotech.png" style="width: 20px;" />
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link href="/css/styles.css" rel="stylesheet">
+</head>
+
+<body id="page-top">
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="/assets/img/logos/aiotech_brand_white.png" alt="" /></a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <i class="fas fa-bars ml-1"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav text-uppercase ml-auto">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#home">Home</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#services">Services</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About Us</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#team">Our Team</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#contact">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Masthead-->
+    <header class="masthead" id="home">
+        <div class="container">
+            <div class="masthead-subheading">Welcome To AioTech!</div>
+            <div class="masthead-heading text-uppercase">It's Nice To Meet You</div>
+        </div>
+    </header>
+    <!-- Services-->
+    <section class="page-section" id="services">
+        <div class="container">
+            <div class="text-center">
+                <h2 class="section-heading text-uppercase">Services</h2>
+            </div>
+            <div class="row text-center">
+                <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                        <i class="fas fa-circle fa-stack-2x text-primary"></i>
+                        <i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
+                    </span>
+                    <h4 class="my-3">E-Commerce</h4>
+                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                </div>
+                <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                        <i class="fas fa-circle fa-stack-2x text-primary"></i>
+                        <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
+                    </span>
+                    <h4 class="my-3">Responsive Design</h4>
+                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                </div>
+                <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                        <i class="fas fa-circle fa-stack-2x text-primary"></i>
+                        <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
+                    </span>
+                    <h4 class="my-3">Web Security</h4>
+                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- About-->
+    <section class="page-section" id="about">
+        <div class="container">
+            <div class="text-center">
+                <h2 class="section-heading text-uppercase">About Us</h2>
+                <h3 class="section-subheading text-muted"></h3>
+            </div>
+            <ul class="timeline">
+                <li>
+                    <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/1.jpg" alt="" /></div>
+                    <div class="timeline-panel">
+                        <div class="timeline-heading">
+                            <h4>2020</h4>
+                            <h4 class="subheading">Our Beginnigs</h4>
+                        </div>
+                        <div class="timeline-body">
+                            <p class="text-muted">AIoTech is a startup company engaged in software development and research in the fields of Artificial Intelligence, Internet of Things and Technology.</p>
+                        </div>
+                    </div>
+                </li>
+                <li class="timeline-inverted">
+                    <div class="timeline-image">
+                        <h4>
+                            Be Part
+                            <br />
+                            Of Our
+                            <br />
+                            Story!
+                        </h4>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <!-- Team-->
+    <section class="page-section bg-light" id="team">
+        <div class="container">
+            <div class="text-center">
+                <h2 class="section-heading text-uppercase">Our Team</h2>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <div class="team-member">
+                        <img class="mx-auto rounded-circle" src="assets/img/team/default.jpg" alt="COM" />
+                        <h4>Haris Manjakawang</h4>
+                        <p class="text-muted">Co-Founder and Commissioner</p>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="team-member">
+                        <img class="mx-auto rounded-circle" src="assets/img/team/default.jpg" alt="CEO" />
+                        <h4>Renaldy Bagas Bayu Pambudi</h4>
+                        <p class="text-muted">Co-Founder and CEO</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="team-member">
+                        <img class="mx-auto rounded-circle" src="assets/img/team/dhika.jpg" alt="CTO" />
+                        <h4>Muhammad Dhika Azizi</h4>
+                        <p class="text-muted">Chief Technology Officer</p>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="team-member">
+                        <img class="mx-auto rounded-circle" src="assets/img/team/default.jpg" alt="CFO" />
+                        <h4>Tiwi</h4>
+                        <p class="text-muted">Chief Financial Officer and Career Dev</p>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="team-member">
+                        <img class="mx-auto rounded-circle" src="assets/img/team/default.jpg" alt="CMO" />
+                        <h4>Sri Aulia Khalifa</h4>
+                        <p class="text-muted">Chief Marketing Officer and Media</p>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <div class="team-member">
+                                <img class="mx-auto rounded-circle" src="assets/img/team/default.jpg" alt="Vice CMO" />
+                                <h4>Alya Febianti</h4>
+                                <p class="text-muted">Vice Chief Marketing Officer and Media</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="team-member">
+                        <img class="mx-auto rounded-circle profile" src="assets/img/team/pepi.JPG" alt="CFO" />
+                        <h4>Pepi Nigatama</h4>
+                        <p class="text-muted">Chief Content Officer</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-8 mx-auto text-center">
+                        <p class="large text-muted">Humans are creatures with extraordinary intellectual intelligence, such as current AI technology which has penetrated into various fields making it easier to apply human thought or intelligence to a computer or system..</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Contact-->
+    <section class="page-section" id="contact">
+        <div class="container">
+            <div class="text-center">
+                <h2 class="section-heading text-uppercase">Contact Us</h2>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <iframe class="maps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.719890623071!2d106.72425701458849!3d-6.168251095534269!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f788f60e492d%3A0x8b5ed89a85136510!2sInstitut%20Teknologi%20PLN!5e0!3m2!1sid!2sid!4v1609942749266!5m2!1sid!2sid" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                </div>
+                <div class="col contact">
+                    <h4 class="contact-head">AioTech ITPLN</h4>
+                    <p>Institut Teknologi PLN</p>
+                    <p>Menara PLN, Jl. Lkr. Luar Barat, RT.1/RW.1, Duri Kosambi</p>
+                    <p>Cengkareng - Kota Jakarta Barat. 11750</p>
+                    <div class="row">
+                        <div class="col-sm-1">
+                            <a class="btn btn-primary btn-social my-4 mx-2" href="https://www.instagram.com/aiotechitpln/?hl=id"><i class="fab fa-instagram"></i></a>
+                        </div>
+                        <div class="col-sm-10 mt-4 ml-3">
+                            <p>Instagram AioTech ITPLN</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Footer-->
+    <footer class="footer py-4">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg text-lg-center font-weight-bold">Copyright © 2020 AioTech ITPLN. All rights reserved</div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- Bootstrap core JS-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Third party plugin JS-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+    <!-- Contact form JS-->
+    <script src="/assets/mail/jqBootstrapValidation.js"></script>
+    <script src="/assets/mail/contact_me.js"></script>
+    <!-- Core theme JS-->
+    <script src="/js/scripts.js"></script>
+</body>
+
+</html>
